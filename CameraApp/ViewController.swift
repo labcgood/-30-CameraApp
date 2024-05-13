93f4f7feb7bc6272c01c696cb2a535889bc96a55
Rabbit <labcgood44@gmail.com>
2024-05-13 14:43:44 +0800

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Labe on 2024/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
